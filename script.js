052c486ff23721f6bbbf561ee3af554d7289e872
Ratkovski <drezcry@hotmail.com>
2021-02-13 14:32:31 -0300

    definir fim do jogo

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function criarBG(){
 
 }
 function criarCobrinha(){
-    for(i=0 <snake.length; i++){
+    for(i=0 <snake.length; i++;){
         context.fillStyle = "green";
         context.fillReact(snake[i].x, snake[i].y, box, box);
 
@@ -29,7 +29,7 @@ function criarCobrinha(){
 }
 
 
-function drawFood{
+function drawFood(){
     context.fillStyle = "red";
     context.fillReact(food.x, food.y, box, box); 
 }
@@ -44,12 +44,23 @@ function update (event){
 }
 
 function iniciarJogo(){
+    
+
+
 
     if(snake[0].x > 15 * box && direction == "right")snake[0].x = 0 ;
     if(snake[0].x < 0   && direction == "left")snake[0].x = 16 * 0 ;
     if(snake[0].y > 15 * box && direction == "down")snake[0].y = 0 ;
     if(snake[0].y < 0  && direction == "up")snake[0].y = 16 * box ;
 
+
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Game Over :(');
+        }
+
+    }
 
 criarBG();
 criarCobrinha();
