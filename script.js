a9ec52709c0bae92306f87786eafe0828c68f9c6
Ratkovski <drezcry@hotmail.com>
2021-02-13 14:07:06 -0300

    make food

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ snake[0] = {
 }
 
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1)* box,
+    y: Math.floor(Math.random() * 15 + 1)* box
+
+}
 
 function criarBG(){
     context.fillStyle = "lightgreen";
@@ -21,6 +26,12 @@ function criarCobrinha(){
 
     }
 
+}
+
+
+function drawFood{
+    context.fillStyle = "red";
+    context.fillReact(food.x, food.y, box, box); 
 }
 
 document.addEventListener('keydown', update);
@@ -42,6 +53,7 @@ function iniciarJogo(){
 
 criarBG();
 criarCobrinha();
+drawFood();
 
 let snakeX = snake[0].x;
 let snakeY = snake[0].y;
